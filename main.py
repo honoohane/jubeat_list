a121bb6e58cecb1a3a3ebdd6c602c98fd9e8e779
Zeng <sheng.zeng@p86898.intra.rakuten.co.jp>
2023-12-08 17:12:50 +0900

    Update b.ave

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from script.export_suzuchan import export_suzuchan
 
 def main():
     # url = 'file:///' + os.path.abspath('.') + '/wikipage.html'
-    url_o = 'https://w.atwiki.jp/jubeat/pages/2224.html'
+    url_o = 'https://w.atwiki.jp/jubeat/pages/2334.html'
     parse_it(url_o)
-    url_n = 'https://w.atwiki.jp/jubeat/pages/2223.html'
+    url_n = 'https://w.atwiki.jp/jubeat/pages/2333.html'
     parse_it(url_n, append=True)
     export_suzuchan()
 
